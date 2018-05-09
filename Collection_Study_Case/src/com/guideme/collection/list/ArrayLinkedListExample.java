e19d9b832d406a9f90d1ace149572e23cca708cd
Arun <guideme360@gmail.com>
2018-05-09 23:43:43 +0530

    Linked List Example

--- Collection_Study_Case/src/com/guideme/collection/list/ArrayLinkedListExample.java
+++ Collection_Study_Case/src/com/guideme/collection/list/ArrayLinkedListExample.java
@@ -3,27 +3,40 @@ package com.guideme.collection.list;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.swing.plaf.synth.SynthStyle;
+
 public class ArrayLinkedListExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<String> list = new LinkedList<>();
+		LinkedList<String> list = new LinkedList<>();
 		list.add("Arun");
 		list.add("Nancy");
-		list.add("Ananth");
+		list.add("Surya");
+		list.add("Jenitha");
+		list.add("Raaji");
+		list.add("");
+		list.add("Surya");
+		list.add(null);
+		list.add(2,"Test");
+		list.addFirst("Add First");
+		list.addLast("Add Last");
 
-		iterateList(list);		
-		list.remove("Nancy");
-		System.out.println("=========Linked List==========");
+		System.out.println("===============| Print Linked List |================");
 		iterateList(list);
-		list.removeAll(list);
-		System.out.println("========================");
-		iterateList(list);
+		System.out.println("Index of :"+list.get(5));
+		System.out.println("Last index of :"+list.get(2));
+		
+		System.out.println("===============| Print Linked List After Clone|================");
+		 LinkedList llist2 = new LinkedList();
+		 llist2 = (LinkedList) list.clone();
+		 llist2.add("New List Element");
+		 iterateList(llist2);
 	}
 
 	private static void iterateList(List<String> list) {
 		for (String s : list) {
-			System.out.println("List Data :"+s);
+			System.out.println("List Data :" + s);
 		}
 	}
 
