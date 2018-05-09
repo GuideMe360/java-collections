1d747e30613046b9e3ad2ded2a225cc79db92106
Arun <guideme360@gmail.com>
2018-05-09 22:42:23 +0530

    New project Added

---
+++ Collection_Study_Case/src/com/collection/list/ArrayLinkedListExample.java
@@ -0,0 +1,30 @@
+package com.collection.list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayLinkedListExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = new LinkedList<>();
+		list.add("Arun");
+		list.add("Nancy");
+		list.add("Ananth");
+
+		iterateList(list);		
+		list.remove("Nancy");
+		System.out.println("=========Linked List==========");
+		iterateList(list);
+		list.removeAll(list);
+		System.out.println("========================");
+		iterateList(list);
+	}
+
+	private static void iterateList(List<String> list) {
+		for (String s : list) {
+			System.out.println("List Data :"+s);
+		}
+	}
+
+}
