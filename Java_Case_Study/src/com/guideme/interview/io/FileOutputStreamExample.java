1befff7a4e943a93abd5cad68630fff621a02f87
Arun <guideme360@gmail.com>
2018-05-23 00:43:05 +0530

    IO Examples

--- Java_Case_Study/src/com/guideme/interview/io/FileOutputStreamExample.java
+++ Java_Case_Study/src/com/guideme/interview/io/FileOutputStreamExample.java
@@ -5,8 +5,10 @@
  */
 package com.guideme.interview.io;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -29,10 +31,10 @@ public class FileOutputStreamExample {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		try {
 			FileOutputStream fos = new FileOutputStream("c:\\dummy\\test.txt");
-			String s ="Welcome to Guideme 360";
+			String s = "Welcome to Guideme 360";
 			byte b[] = s.getBytes();
 			fos.write(b);
 			fos.close();
@@ -40,6 +42,27 @@ public class FileOutputStreamExample {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		try {
+			File fos = new File("c:\\dummy\\test.txt");
+			if (!fos.exists()) {
+				fos.createNewFile();
+			}
+			fw = new FileWriter(fos, true);
+			bw = new BufferedWriter(fw);
+			bw.write("\nAdded Additional Content to Test 01");
+			System.out.println("File Updated Successfully....");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			bw.close();
+			fw.close();
 		}
 	}
 
